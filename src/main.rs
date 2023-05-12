2cebc9a1320e4c607fcb7429d4f3b69969be1f7c
aashish2057 <aashish2057@gmail.com>
2023-05-12 14:51:22 -0700

    clean up and setup read cli

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use clap::{arg, Command};
 fn cli() -> Command {
     Command::new("rcsvd")
         .about("Parse CSVs into DB Tables")
+        .subcommand_required(false)
         .arg_required_else_help(true)
-        .subcommand(Command::new("test").about("testing this out"))
+        .subcommand(
+            Command::new("read")
+                .about("Read and print out csv")
+                .arg(arg!(<PATH> "The path to the csv file"))
+                .arg_required_else_help(true),
+        )
 }
 
 fn main() {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
-        Some(("test", matches)) => {
-            println!("WEEEEE");
+        Some(("read", matches)) => {
+            let path = matches.get_one::<String>("PATH").expect("Path is missing");
+            println!("Path: {}", path);
         }
         _ => unreachable!(),
     }
